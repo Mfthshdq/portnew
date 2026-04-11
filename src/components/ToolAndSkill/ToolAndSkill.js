2a4bfa5cc7885a30a0594383cccd6bf26191db33
Mfthshdq <muhammadmiftahusshiddiq@gmail.com>
2026-04-11 20:14:54 +0700

    finish it

--- src/components/ToolAndSkill/ToolAndSkill.js
+++ src/components/ToolAndSkill/ToolAndSkill.js
@@ -3,11 +3,11 @@ import style from "./ToolAndSkill.module.css"
 function ToolAndSkill() {
     return (
         <div className={style.container}>
-            <h1 className={style.title}>Tools And <span style={{color: "#ff4c60"}}>Skills</span></h1>
+            <h1 className={style.title}>Tools And <span style={{ color: "#ff4c60" }}>Skills</span></h1>
 
             <div className={style.toolsandskillslist}>
 
-                <div className={style.toolskill}>
+                {/* <div className={style.toolskill}>
                     <div className={style.tool}>
                         <img src="image/toolsandskills/canva.png" alt="canva" className={style.image}></img>
                     </div>
@@ -47,15 +47,57 @@ function ToolAndSkill() {
                         <img src="image/toolsandskills/figma.png" alt="figma" className={style.image}></img>
                     </div>
                     <h1>Figm<span style={{color: "#ff4c60"}}>A</span></h1>
+                </div> */}
+
+                <div className={style.toolskill}>
+                    <div className={style.tool}>
+                        <img src="image/toolsandskills/html.png" alt="html" className={style.image}></img>
+                    </div>
+                    <h1>HTML</h1>
+                </div>
+
+                <div className={style.toolskill}>
+                    <div className={style.tool}>
+                        <img src="image/toolsandskills/css.png" alt="css" className={style.image}></img>
+                    </div>
+                    <h1>CSS</h1>
+                </div>
+
+                <div className={style.toolskill}>
+                    <div className={style.tool}>
+                        <img src="image/toolsandskills/react.png" alt="react" className={style.image}></img>
+                    </div>
+                    <h1>REACT</h1>
+                </div>
+
+                <div className={style.toolskill}>
+                    <div className={style.tool}>
+                        <img src="image/toolsandskills/laravel.png" alt="laravel" className={style.image}></img>
+                    </div>
+                    <h1>Laravel</h1>
+                </div>
+
+                <div className={style.toolskill}>
+                    <div className={style.tool}>
+                        <img src="image/toolsandskills/figma.png" alt="figma" className={style.image}></img>
+                    </div>
+                    <h1>Figma</h1>
+                </div>
+
+                <div className={style.toolskill}>
+                    <div className={style.tool}>
+                        <img src="image/toolsandskills/canva.png" alt="canva" className={style.image}></img>
+                    </div>
+                    <h1>Canva</h1>
                 </div>
 
             </div>
 
-            <div className={style.marquee}>
+            {/* <div className={style.marquee}>
                 <p>GG GA CELLL ANJAYYY</p>
                 <p>GG KANNN</p>
                 <p>GASI B AJA WKWKWKWK</p>
-            </div>
+            </div> */}
 
         </div>
     )
